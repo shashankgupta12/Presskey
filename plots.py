2499dae9e0b2a50f2e72e512f691d6fefbfbf6d4
Shashank Gupta <shashankgupta1994@gmail.com>
2017-07-08 17:59:47 +0530

    added units to axis of graph

--- plots.py
+++ plots.py
@@ -15,7 +15,7 @@ def plotOfBoxplots():
 	plt.boxplot(ikt, vert=0)
 	my_yticks = ['s-t', 't-u', 'u-d', 'd-e', 'e-n', 'n-t', 't-space', 'space-h', 'h-e', 'e-l', 'l-l', 'l-o', 'o-space', 'space-w', 'w-o', 'o-r', 'r-l', 'l-d']
 	plt.yticks(list(range(1, 19)), my_yticks)
-	plt.xlabel('Range of interkey values')
+	plt.xlabel('Range of interkey values (*10^4)(microseconds)')
 	plt.ylabel('Box plots')
 	plt.show()
 
@@ -23,8 +23,8 @@ def profilePlot():
 	all_keyHoldTime = []
 	all_interkeyTime = []
 	
-	for user in range(1, 12):
-		for text in range(1, 5):
+	for user in range(1, 2):
+		for text in range(1, 2):
 			filename = 'processedData/text{0}/user{1}.json'.format(text, user)
 			with open(filename, 'r') as f:
 				data = [json.loads(line) for line in f]
@@ -47,8 +47,8 @@ def profilePlot():
 				plt.figure(fig)
 				plt.plot([i/10000 for i in kht], [i/10000 for i in ikt])
 		
-			plt.xlabel('Key Hold Time (*10^4)(sec)')
-			plt.ylabel('Interkey Time (*10^4)(sec)')
+			plt.xlabel('Key Hold Time (*10^4)(microseconds)')
+			plt.ylabel('Interkey Time (*10^4)(microseconds)')
 			plt.savefig('user{}-text{}.png'.format(user, text))
 
 if __name__ == '__main__':
